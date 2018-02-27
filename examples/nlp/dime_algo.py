9f7e0e17315f012371acc1e9f9abfbf202434098
Lev Kusanagi <levkusanagi@gmail.com>
2018-02-27 03:56:07 +0100

    Remove urllib import.

--- examples/nlp/dime_algo.py
+++ examples/nlp/dime_algo.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import pickle
-from six.moves import urllib
+# from six.moves import urllib
 
 import tflearn
 from tflearn.data_utils import *
